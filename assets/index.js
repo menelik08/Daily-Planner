4433edb023b95cebd3eede78cc0849dac7b27422
Yosef Teshome <menelik08@gmail.com>
2022-11-28 20:57:07 -0500

    js, index, styles, ReadMe added with code

---
+++ assets/index.js
@@ -0,0 +1,71 @@
+//Javascript for Daily Scheduler
+
+// GIVEN I am using a daily planner to create a schedule
+//1) 
+    // WHEN I open the planner
+    // THEN the current day is displayed at the top of the calendar
+    // WHEN I scroll down
+//2)
+    // THEN I am presented with time blocks for standard business hours
+    // WHEN I view the time blocks for that day
+    // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+//3
+    // WHEN I click into a time block
+    // THEN I can enter an event
+
+//4
+    // WHEN I click the save button for that time block
+    // THEN the text for that event is saved in local storage
+    // WHEN I refresh the page
+    // THEN the saved events persist
+
+// Setting header date, retrieving stored data from local storage
+function setPlanner() {
+
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        //Saving to local storage
+        var schedule = localStorage.getItem(id);
+        //For each time block, it will have the respecting scheduled values 
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    });
+}
+
+setPlanner();
+var saveBtn = $(".saveBtn");
+
+// When save btn is clicked, each time blocks' attributes and values are stored to local storage
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+    localStorage.setItem(time, schedule);
+});
+
+function pastPresentFuture() {
+    var currentHour = parseInt(moment().format("HH"));
+    // console.log("What is the current hour:", currentHour);
+
+    //Grabbing the hour that each time block represents, we then compare to the current time, to display the correct colour-code on each time block.
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+
+        if (thisHour > currentHour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === currentHour) {
+            $(this).addClass("present");
+        }
+        else if (thisHour < currentHour){
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
+
+
+
